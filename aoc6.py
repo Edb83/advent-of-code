6a05d3ceec7e5270b62740ddf5626b7238195506
Ed Bradley <ed3radley@gmail.com>
2022-12-06 09:29:33 +0000

    Complete aoc6

--- aoc6.py
+++ aoc6.py
@@ -2,3 +2,14 @@ import env
 from aocd.models import Puzzle
 
 puzzle = Puzzle(year=2022, day=6)
+
+def chars_to_match(n):
+    letters = puzzle.input_data
+    current = []
+    for i in range(n, len(letters) + n):
+        current = letters[i-n:i]
+        if len(set(current)) == len(current):
+            return i
+
+print(chars_to_match(4))
+print(chars_to_match(14))
